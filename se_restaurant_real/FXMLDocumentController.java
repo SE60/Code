9295e62c9976b9170efb3255c17005ca0d04b714
l33oat <iii-boat@hotmail.com>
2017-10-25 02:03:44 +0700

    Delete Pratice

--- se_restaurant_real/FXMLDocumentController.java
+++ se_restaurant_real/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package se_restaurant;
+package se_restaurant_real;
 
 import java.net.URL;
 import java.util.ResourceBundle;
