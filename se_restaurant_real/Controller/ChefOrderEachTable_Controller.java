29cb74b9050a2f8d432327cab019be478f3b2324
TryAgain <adventure_39@hotmail.com>
2017-11-15 03:58:13 +0700

    Cancle Button Complete

--- se_restaurant_real/Controller/ChefOrderEachTable_Controller.java
+++ se_restaurant_real/Controller/ChefOrderEachTable_Controller.java
@@ -6,15 +6,23 @@
 package se_restaurant_real.Controller;
 
 import RestaurantDB.Ordered;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+import se_restaurant_real.Main;
+import static se_restaurant_real.Main.cOrder;
 /**
  *
  * @author TryAgain
@@ -27,6 +35,10 @@ public class ChefOrderEachTable_Controller {
     private List<Label> menuViewList;
     @FXML
     private List<CheckBox> checkViewList;
+    @FXML
+    private Button confirmButton;
+    @FXML
+    private Button cancleButton;
     
     @FXML
     protected void initialize(){
@@ -37,10 +49,30 @@ public class ChefOrderEachTable_Controller {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("restaurant.odb");
         EntityManager em=emf.createEntityManager();
         TypedQuery<Ordered> q1=em.createQuery("select ca from Ordered ca",Ordered.class);
+        table.setText(Integer.toString(q1.getResultList().get(0).getTable()));
         for (int i=0; i<q1.getResultList().get(0).getPrice().size(); i++){
             menuViewList.get(i).setVisible(true);
             checkViewList.get(i).setVisible(true);
             menuViewList.get(i).setText(q1.getResultList().get(0).getName().get(i));
         } 
+    }
+    
+    @FXML
+    void cancleButton_Clicked(MouseEvent event) {
+        try {
+             FXMLLoader loader;
+             loader = new FXMLLoader();
+             loader.setLocation(getClass().getResource("/GUI/src/sample/ChefTableOrder.fxml"));
+             cOrder = loader.load();
+            // ---------------------------------------------------------------------------------------
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Main.mainStage.getScene().setRoot(Main.cOrder);
+    }
+
+    @FXML
+    void confirmButton_Clicked(MouseEvent event) {
+
     }
 }
